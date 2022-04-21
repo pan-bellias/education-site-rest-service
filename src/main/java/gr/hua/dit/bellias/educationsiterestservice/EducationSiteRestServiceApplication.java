3155a05fbc44b5cbdfb8ba749607332bb17c5b74
pan-bellias <belliaspan@gmail.com>
2022-04-21 20:24:18 +0300

    spring init

---
+++ src/main/java/gr/hua/dit/bellias/educationsiterestservice/EducationSiteRestServiceApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.bellias.educationsiterestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationSiteRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationSiteRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/hua/dit/bellias/educationsiterestservice/EducationSiteRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.bellias.educationsiterestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSiteRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
